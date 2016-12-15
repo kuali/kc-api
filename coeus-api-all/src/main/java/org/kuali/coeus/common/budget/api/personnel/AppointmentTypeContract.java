96b733b3f286c1a81fb4e231a054c0f97ffd9c85
Travis Schneeberger <leo.herbie@gmail.com>
2016-12-15 15:24:58 -0500

    RESKC-1729: changing appointment type to use a decimal type for duration

--- coeus-api-all/src/main/java/org/kuali/coeus/common/budget/api/personnel/AppointmentTypeContract.java
+++ coeus-api-all/src/main/java/org/kuali/coeus/common/budget/api/personnel/AppointmentTypeContract.java
@@ -20,8 +20,9 @@ package org.kuali.coeus.common.budget.api.personnel;
 
 import org.kuali.coeus.sys.api.model.Coded;
 import org.kuali.coeus.sys.api.model.Describable;
+import org.kuali.coeus.sys.api.model.ScaleTwoDecimal;
 
 public interface AppointmentTypeContract extends Coded, Describable {
 
-    Integer getDuration();
+    ScaleTwoDecimal getDuration();
 }
