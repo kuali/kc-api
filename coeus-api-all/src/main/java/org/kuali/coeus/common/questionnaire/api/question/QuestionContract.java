0f4d6a8844cd63c6c31adcc27beb00fe28b3d11a
Travis Schneeberger <leo.herbie@gmail.com>
2019-02-04 18:59:49 -0500

    License (#62)
    * RESKC-3416: adding maven profile and configuration to generate licence header
    
    * RESKC-3416: removing existing license headers

--- coeus-api-all/src/main/java/org/kuali/coeus/common/questionnaire/api/question/QuestionContract.java
+++ coeus-api-all/src/main/java/org/kuali/coeus/common/questionnaire/api/question/QuestionContract.java
@@ -1,10 +1,4 @@
-/* Copyright © 2005-2018 Kuali, Inc. - All Rights Reserved
- * You may use and modify this code under the terms of the Kuali, Inc.
- * Pre-Release License Agreement. You may not distribute it.
- *
- * You should have received a copy of the Kuali, Inc. Pre-Release License
- * Agreement with this file. If not, please write to license@kuali.co.
- */
+
 package org.kuali.coeus.common.questionnaire.api.question;
 
 import org.kuali.coeus.common.questionnaire.api.core.QuestionTypeContract;
