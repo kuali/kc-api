c9803bff61edf731e06b2ca6a05f5c5c9922eabd
jenkins <jenkins@kuali.co>
2019-02-04 17:52:10 -0700

    Update License Headers #norelease #nobuild

--- coeus-api-all/src/main/java/org/kuali/coeus/propdev/api/budget/modular/BudgetModularContract.java
+++ coeus-api-all/src/main/java/org/kuali/coeus/propdev/api/budget/modular/BudgetModularContract.java
@@ -1,3 +1,15 @@
+/*-
+ * #%L
+ * %%
+ * Copyright (C) 2014 - 2019 Kuali, Inc. - All Rights Reserved
+ * %%
+ * You may use and modify this code under the terms of the Kuali, Inc.
+ * Pre-Release License Agreement. You may not distribute it.
+ * 
+ * You should have received a copy of the Kuali, Inc. Pre-Release License
+ * Agreement with this file. If not, please write to license@kuali.co.
+ * #L%
+ */
 
 package org.kuali.coeus.propdev.api.budget.modular;
 
